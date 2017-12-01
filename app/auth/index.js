5eba837faee28e66cd0c5a07a8fc8dbac3edcd63
Trịnh Mười <trinhmuoi@hiworld.com.vn>
2017-12-01 09:48:01 +0700

    update eslint

--- app/auth/index.js
+++ app/auth/index.js
@@ -1,27 +1,27 @@
 var passport = require('passport'),
-  passportJWT = require('passport-jwt'),
-  config = require('../../config/config'),
-  extractJwt = passportJWT.ExtractJwt,
-  strategyJwt = passportJWT.Strategy,
-  auth = config.auth
+    passportJWT = require('passport-jwt'),
+    config = require('../../config/config'),
+    extractJwt = passportJWT.ExtractJwt,
+    strategyJwt = passportJWT.Strategy,
+    auth = config.auth
 const db = require('../models')
 const userService = require('../services/user')(db.sequelize)
 
 const params = {
-  secretOrKey: auth.secretOrKey,
-  jwtFromRequest: extractJwt.fromAuthHeaderWithScheme('jwt')
+    secretOrKey: auth.secretOrKey,
+    jwtFromRequest: extractJwt.fromAuthHeaderWithScheme('jwt')
 }
 
 var jwtStrategy = new strategyJwt(params, function (payload, done) {
-  userService.checkPayload(payload.id, function (error, result) {
-    if(error) {
-      return done('Not access')
-    }
-    if (!result) {
-      return done('Not access')
-    }
-    return done(null, { id: payload.id })
-  })
+    userService.checkPayload(payload.id, function (error, result) {
+        if (error) {
+            return done('Not access')
+        }
+        if (!result) {
+            return done('Not access')
+        }
+        return done(null, { id: payload.id })
+    })
 })
 passport.use(jwtStrategy)
 // module.exports.initialize = passport.initialize()
