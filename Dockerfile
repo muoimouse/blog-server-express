76ac9c4f181156ae2b322532827fcc32dd573ea0
muoitm <muoimouse@gmail.com>
2018-01-03 17:07:38 +0700

    add service docker nginx

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM node:8.9.0
+FROM node:8.9.3
 # Create app directory
 RUN mkdir -p /usr/src/blog-server-express
 
@@ -12,4 +12,4 @@ WORKDIR /usr/src/blog-server-express
 RUN npm set progress false && npm install
 
 EXPOSE 1337
-CMD [ "gulp" ]
+ CMD [ "gulp" ]
--- app/controllers/articlesController.js
+++ app/controllers/articlesController.js
@@ -1,4 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../models');
-const articleService = require('../services/')
+const articleService = require('../services/article')(db.sequelize);
+const authenticate = require('../auth/checkToken');
+
+const errorCodes = {
+
+};
+
+module.exports = (app) => {
+  app.use('/article', router);
+};
